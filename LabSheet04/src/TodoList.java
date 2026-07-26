c3b22e6410e66fc09700c1dbd908edbac22c8f65
Teeranut-hub <kirikaya085@gmail.com>
2026-07-26 18:55:06 +0700

    LabSheet04

---
+++ LabSheet04/src/TodoList.java
@@ -0,0 +1,32 @@
+
+public class TodoList {
+
+	public static void main(String[] args) {
+		DoublyLinkedList todoList = new DoublyLinkedList();
+		todoList.insert("Finish Homework");
+		System.out.println(todoList.traversal());
+		
+		todoList.insert("Laundry");
+		todoList.insert("Group Meeting");
+		System.out.println(todoList.traversal());
+		// Exercise no.2.1
+		todoList.insert(0, "Submit Report");
+		System.out.println(todoList.traversal());
+		// Exercise no.2.2
+		todoList.insert(2, "Buy Food");
+		System.out.println(todoList.traversal());
+		// Exercise no.2.3
+		todoList.insert("Go to Gym");
+		System.out.println(todoList.traversal());
+		todoList.remove();
+		
+		todoList.remove(3);
+		
+		todoList.remove(0);
+		
+		System.out.println("To-Do List = " + todoList.traversal());
+		
+		System.out.println("\nBackward Traversal = " + todoList.backwardTraversal());
+	}
+
+}
